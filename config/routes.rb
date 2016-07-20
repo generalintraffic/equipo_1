0ac47c44b67c77cadd79ce115f12b9c680d17ca0
projas9284 <pedrorojas89@gmail.com>
2016-07-20 10:06:56 -0400

    [#126591831] add routes Queries Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'queries#index'
+  get '/cars' => 'queries#search'
+  get '/cars/(:id)' => 'queries#simulation'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

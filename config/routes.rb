abf6261293ac2ee67053729a664fc0e29b6e81d4
projas9284 <pedrorojas89@gmail.com>
2016-07-22 09:21:09 -0400

    Delete Controlador mapa

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  root 'mapa#index'
-
   devise_for :users
   root 'queries#index'
   get '/cars' => 'queries#search'
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20160720031534) do
-=======
 ActiveRecord::Schema.define(version: 20160720134055) do
 
   create_table "queries", force: :cascade do |t|
@@ -27,7 +24,6 @@ ActiveRecord::Schema.define(version: 20160720134055) do
   end
 
   add_index "queries", ["user_id"], name: "index_queries_on_user_id"
->>>>>>> tyrell
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false

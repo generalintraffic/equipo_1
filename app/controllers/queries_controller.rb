1d922461435bdd797be40b4effb366f2956ddd8c
projas9284 <pedrorojas89@gmail.com>
2016-07-25 09:23:14 -0400

    avances en backend con Argel

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -1,4 +1,13 @@
 class QueriesController < ApplicationController
   def index
   end
+
+  def search
+    @query = Query.new
+    @query.get_token
+    @query.get_vehicles_in_zone #con los parametros de Argel
+    @query.save
+    @cars = @query.cars
+    render json: @cars #son solo los id
+  end
 end

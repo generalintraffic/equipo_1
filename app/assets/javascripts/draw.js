9a850e1fc652c652dcf8ddd5367222f565347f09
abiasco3047 <argelbiascoayala20@gmail.com>
2016-07-25 20:58:06 -0430

    lista de vehiculos ajax

--- app/assets/javascripts/draw.js
+++ app/assets/javascripts/draw.js
@@ -1,46 +1,54 @@
-// var featureGroup = L.featureGroup().addTo(map);
-// var drawControl = new L.Control.Draw({
-//   edit: {
-//     featureGroup: featureGroup
-//   },
-//   draw: {
-//     polygon: false,
-//     polyline: false,
-//     rectangle: true,
-//     circle: true,
-//     marker: false
-//   }
-// }).addTo(map);
-//
-// map.on('draw:created', showPolygonArea);
-// map.on('draw:edited', showPolygonAreaEdited);
-//
-// function showPolygonAreaEdited(e) {
-//   e.layers.eachLayer(function(layer) {
-//     showPolygonArea({ layer: layer });
-//   });
-// }
-// function showPolygonArea(e) {
-//   featureGroup.clearLayers();
-//   featureGroup.addLayer(e.layer);
-//   e.layer.bindPopup((LGeo.area(e.layer) / 1000000).toFixed(2) + ' km<sup>2</sup>');
-//   e.layer.openPopup();
-// }
-// var RADIUS = 300;
-// var filterCircle = L.circle(L.latLng(0, 0), RADIUS, {
-//     opacity: 1,
-//     weight: 1,
-//     fillOpacity: 0.4,
-//     color: 'green'
-// }).addTo(map);
-//
-// var csvLayer = omnivore.csv('DATA', null, L.mapbox.featureLayer()).addTo(map);
-//
-// map.on('click', function(e) {
-//     filterCircle.setLatLng(e.latlng);
-//     csvLayer.setFilter(function showAirport(feature) {
-//         return e.latlng.distanceTo(L.latLng(
-//                 feature.geometry.coordinates[1],
-//                 feature.geometry.coordinates[0])) < RADIUS;
-//     });
-// });
+var featureGroup = L.featureGroup().addTo(map);
+var drawControl = new L.Control.Draw({
+  edit: {
+    featureGroup: featureGroup
+  },
+  draw: {
+    polygon: false,
+    polyline: false,
+    rectangle: true,
+    circle: false,
+    marker: false
+  }
+}).addTo(map);
+
+map.on('draw:created', showPolygonArea);
+map.on('draw:edited', showPolygonAreaEdited);
+
+var csvLayer = omnivore.csv('https://api.intraffic.com.ve/routing.geojson', null, L.mapbox.featureLayer()).addTo(map);
+
+function showPolygonAreaEdited(e) {
+  e.layers.eachLayer(function(layer) {
+    showPolygonArea({ layer: layer });
+  });
+}
+function showPolygonArea(e) {
+    layer = e.layer;
+    var latlngs = (layer.getLatLngs());
+    var diagonal = [latlngs[1], latlngs[3]];
+     console.log(diagonal);
+  featureGroup.clearLayers();
+  featureGroup.addLayer(e.layer);
+  e.layer.bindPopup((LGeo.area(e.layer) / 1000000).toFixed(2) + ' km<sup>2</sup>');
+  e.layer.openPopup();
+   $.ajax({
+  type: "POST",
+  url: "/cars_in_zone.json",
+  success:  function(data) {
+   console.log(data)
+   data.forEach(function(car_id) {
+          $("#sidebar").append(
+          '<div class="item" id="' + car_id + '">CarName ' + car_id +'</div>');
+;
+        });
+    }
+});
+
+  csvLayer.setFilter(function showCars(feature) {
+      return e.latlng.distanceTo(L.latLng(
+              feature.geometry.coordinates[1],
+              feature.geometry.coordinates[0]));
+  });
+  }
+
+route_marker = L.marker([element.geometry.coordinates[0][1], element.geometry.coordinates[0][0]],{draggable:true}).addTo(mymap)

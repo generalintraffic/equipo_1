952147d53b42d03a0034d48286c941b169402383
projas9284 <pedrorojas89@gmail.com>
2016-07-30 05:45:13 -0400

    [#126589779] Modelo Back end listo

--- app/models/query.rb
+++ app/models/query.rb
@@ -18,11 +18,11 @@
 class Query < ActiveRecord::Base
   belongs_to :user
 
-  # Las columnas :area, :cars, :positions,:routes ahora son String-Array
+  # Las columnas :area, :cars, :positions ahora son String-Array
   serialize :area, Array
   serialize :cars, Array
   serialize :positions, Array
-  serialize :routes
+  serialize :routes #routes es un hash
 
   # Lógica para connsumir Api InTraffic
 
@@ -104,7 +104,7 @@ class Query < ActiveRecord::Base
     return self
   end
 
-  def get_vehicle_position(vehicle_id) #ERROR API INTRAFFIC Siempre obtiene las mismas coordenadas
+  def get_vehicle_position(vehicle_id) 
     self.check_token
     self.selected_car = vehicle_id
     url = URI('https://api.intraffic.com.ve/vehicles/get_vehicle_position.json/?vehicle_id=' + vehicle_id) 
@@ -118,18 +118,27 @@ class Query < ActiveRecord::Base
     request["cache-control"] = "no-cache"
     http_response = http.request(request)
     response = eval(http_response.read_body)
-    self.positions.push(response) #cuando funcione hay que asignar y no pushear
+    self.positions = response
     puts response
     return self
   end
 
-  def get_route #Falta arreglar para utilice self.positions
+  def get_route
     self.check_token
-    long1 = self.area[0]["lng"].to_s
-    lat1 = self.area[0]["lat"].to_s
-    long2 = self.area[1]["lng"].to_s
-    lat2 = self.area[1]["lat"].to_s
-    url = URI('https://api.intraffic.com.ve/routing.geojson?points[]='+long1+','+lat1+'&points[]='+long2+','+lat2)
+    url_base = 'https://api.intraffic.com.ve/routing.geojson?'
+    url_query = ''
+    # array_coordinates = []
+    self.positions[0][:lon_lat].each do |coordinate|
+      array_string = []
+      array_string = coordinate.split(',')
+      url_query += 'points[]=' + array_string[1]+','+array_string[0] + '&'
+      # array_coordinates.push(array_string[1])
+      # array_coordinates.push(array_string[0])
+    end
+    #self.positions[0][:coordinates] = array_coordinates
+    url_query = url_query[0..url_query.length-2]
+    self.positions[0][:query_to_do] = url_query
+    url = URI(url_base+url_query)
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
@@ -147,18 +156,22 @@ class Query < ActiveRecord::Base
     return self
   end
 
-  #metodo para 
+  #metodo para agregar a cada link un array de coordenadas en formato lat-long 
 
   def fixing_routes
     @routes = self.routes
     @routes["features"].each do |link|
+      link["geometry"]["lat-long"]=[]
       link["geometry"]["coordinates"].each do |coordinate|
-        mem = coordinate[1]
-        coordinate[1] = coordinate[0]
-        coordinate[0] = mem
+        aux_array = []
+        aux_array[0] = coordinate[1]
+        aux_array[1] = coordinate[0]
+        link["geometry"]["lat-long"].push(aux_array)
       end
     end
   end
+
+  #metodo para agregar a cada link["properties"] un tiempo de trayecto en miliseconds
 
   def adding_miliseconds
     @route = self.routes
@@ -169,7 +182,7 @@ class Query < ActiveRecord::Base
 
 end
 
-  #Varios vehiculos
+  #get Varios vehiculos
 
   # def get_vehicles_position #Siempre obtiene las mismas coordenadas
   #   url_base = 'https://api.intraffic.com.ve/vehicles/get_vehicle_position.json/?vehicle_id='
@@ -191,4 +204,28 @@ end
   #   return self
   # end
 
-  #Un solo vehiculo
+  #Metodo viejo de Routes
+
+  # def get_route_2 #Este es el metodo truqueado
+  #   self.check_token
+  #   long1 = self.area[0]["lng"].to_s
+  #   lat1 = self.area[0]["lat"].to_s
+  #   long2 = self.area[1]["lng"].to_s
+  #   lat2 = self.area[1]["lat"].to_s
+  #   url = URI('https://api.intraffic.com.ve/routing.geojson?points[]='+long1+','+lat1+'&points[]='+long2+','+lat2)
+  #   http = Net::HTTP.new(url.host, url.port)
+  #   http.use_ssl = true
+  #   http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  #   request = Net::HTTP::Get.new(url)
+  #   request["user-agent"] = "develop"
+  #   request["authorization"] = "Bearer " + self.token
+  #   request["cache-control"] = "no-cache"
+  #   http_response = http.request(request)
+  #   puts http_response.read_body
+  #   response_string = http_response.read_body
+  #   response = JSON.parse(response_string)
+  #   self.routes = response
+  #   self.fixing_routes
+  #   self.adding_miliseconds
+  #   return self
+  # end
